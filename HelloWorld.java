08150871f3bc1d438b7662d733089fa66b7f133c
Mrityunjay <mrutyunjaya.sahu@omniwyse.com>
2017-08-12 21:25:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
 
 public static void main(String[] args){
-System.out.println("Hello World1");
+System.out.println("Hello World0");
 System.out.println("Hello World2");
 System.out.println("Hello World3");
 }
